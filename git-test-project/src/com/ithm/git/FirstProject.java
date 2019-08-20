b3ec039fca02ba7f07ab42d2b1505bfed5af710b
LuChuYan <280061667@qq.com>
2019-08-20 22:05:52 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/ithm/git/FirstProject.java
@@ -0,0 +1,10 @@
+package com.ithm.git;
+
+public class FirstProject {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
