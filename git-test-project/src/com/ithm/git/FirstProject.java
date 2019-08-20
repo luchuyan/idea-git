e96f4088ae843855e5d864505d83fc3547cd1e39
LuChuYan <280061667@qq.com>
2019-08-20 22:28:33 +0800

    add

--- git-test-project/src/com/ithm/git/FirstProject.java
+++ git-test-project/src/com/ithm/git/FirstProject.java
@@ -6,6 +6,7 @@ public class FirstProject {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git222");
 
     }
 }
